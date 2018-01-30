1128616aa78781c7bc3a195c799a0ce646acea51
Ferdinandvanderwerf <34487619+Ferdinandvanderwerf@users.noreply.github.com>
2018-01-30 17:34:45 +0100

    Add files via upload

---
+++ opdrachtweek3a.php
@@ -0,0 +1,9 @@
+<?php
+
+$t = date("M");
+if ($t == "July") {
+    echo "It's July, so it can get really hot.";
+  } else {
+    echo "It’s not July, so at least we’re not in the peak of the heat.";
+}
+?>
